273afb0ad121213ccfdcb8eceeec26302d9b5553
fred-olsson <olsson.fred@gmail.com>
2016-08-08 15:05:48 +0200

    Color2Word Controller and View

--- MiniProject_VerifiedProject/Controllers/HomeController.cs
+++ MiniProject_VerifiedProject/Controllers/HomeController.cs
@@ -69,6 +69,15 @@ namespace MiniProject_VerifiedProject.Controllers
             return Json(json, JsonRequestBehavior.AllowGet);
         }
 
+        public JsonResult Category_Three_Question_Loader()
+        {
+            Repository repo = new Repository();
+
+            var model = repo.GetAllColor2Words().ToList();
+            var json = new JavaScriptSerializer().Serialize(model);
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
         //Bellow, Controller actions I set for each category separately
         //This action shall return a partial view to be rendered in the main Board
         public PartialViewResult Category_One()
@@ -80,7 +89,12 @@ namespace MiniProject_VerifiedProject.Controllers
         {
             return PartialView("_Category_Two");
         }
-        
+
+        public PartialViewResult Category_Three()
+        {
+            return PartialView("_Category_Three");
+        }
+
         //edited from github
         //test fred
     }
--- MiniProject_VerifiedProject/Scripts/app.js
+++ MiniProject_VerifiedProject/Scripts/app.js
@@ -53,6 +53,23 @@
         }
     });
 
+    //Creates a service to fetch the question data for category three
+    var CategoryThreeLoader = miniProject.service('CategoryThreeLoader', function ($http, $rootScope) {
+        this.promise = null;
+        function makeRequest() {
+            return $http.get('/Home/Category_Three_Question_Loader')
+              .then(function (resp) {
+                  return resp.data;
+              });
+        }
+        this.getPromise = function (update) {
+            if (update || !this.promise) {
+                this.promise = makeRequest();
+            }
+            return this.promise;
+        }
+    });
+
     //Controller for the index page
     var indexCtrl = miniProject.controller("IndexCtrl", function ($window, $scope, $http) {
         $scope.categoryNumber = 0;
@@ -193,5 +210,125 @@
     //Controller for the first game category
     var categoryTwo = miniProject.controller("CategoryTwoCtrl", function ($scope, $timeout, $window, $http, CategoryTwoLoader) {
         //Scope variables
+    });
+
+//Controller for the third game category
+    var categoryThree = miniProject.controller("CategoryThreeCtrl", function ($scope, $timeout, $window, $http, CategoryThreeLoader) {
+    //Scope variables
+    $scope.question;
+    $scope.answer = "";
+    $scope.success = false;
+    $scope.failure = false;
+    $scope.disabled = false;
+    $scope.gameEnded = false;
+    $scope.isReady = false;
+    $scope.score = 0;
+    $scope.results = [];
+    $scope.totalQuestions = 0;
+    $scope.counter = 1;
+    $scope.maxScore = 0;
+
+    //Local variables
+    var questionIndex = 0;
+    var questionsArray = [];
+    var result = {
+        questionNumber: 0,
+        givenAnswer: '',
+        correctAnswer: '',
+        mark: '',
+        color: ''
+    };
+
+    
+
+    //Fetches next question
+    var nextQuestion = function (skipped) {
+        if ($scope.counter >= $scope.totalQuestions) {
+            $scope.gameEnded = true;
+            console.log("no more questions");
+            return;
+        }
+        //if the current question has been skipped
+        if (skipped)
+        {
+            result = {
+                questionNumber: $scope.counter,
+                givenAnswer: '',
+                correctAnswer: $scope.question.ColorName,
+                mark: 'Skipped',
+                color: 'warning'
+            }
+
+            $scope.results.push(result);
+        }
+
+        questionIndex++;
+        $scope.counter++;
+        $scope.disabled = false;
+        $scope.success = false;
+        $scope.failure = false;
+        $scope.question = questionsArray[questionIndex];
+        $scope.answer = '';
+    };
+
+    //Quits game to home screen
+    var quit = function () {
+        $window.location.href = "/Home/Index";
+    };
+
+    //Loading questionnaire
+    CategoryThreeLoader.getPromise().then(function (data) {
+        questionsArray = angular.fromJson(data);
+        $scope.totalQuestions = questionsArray.length;
+        $scope.question = shuffleQuestions(questionsArray)[questionIndex];
+        $scope.maxScore = $scope.totalQuestions * 2;
+        $scope.isReady = true;
+    });
+
+    $scope.checkAnswer = function () {
+        if ($scope.answer.toLowerCase() === $scope.question.ColorName.toLowerCase()) {
+            $scope.success = true;
+            $scope.failure = false;
+            _Score+=2;
+            $scope.score = _Score;
+            result = {
+                questionNumber: $scope.counter,
+                givenAnswer: $scope.answer,
+                correctAnswer: $scope.question.ColorName,
+                mark: 'Correct!',
+                color: 'success'
+            }
+
+            $scope.results.push(result);
+            console.log("success");
+
+        }
+        else {
+            $scope.success = false;
+            $scope.failure = true;
+            result = {
+                questionNumber: $scope.counter,
+                givenAnswer: $scope.answer,
+                correctAnswer: $scope.question.ColorName,
+                mark: 'Incorrect!',
+                color: 'danger'
+            }
+
+            $scope.results.push(result);
+            console.log("failure");
+        }
+
+        $scope.disabled = true;
+        $timeout(nextQuestion, 3000);
+        console.log($scope.question);
+    };
+
+    //Reloads the current game
+    $scope.reload = function () {
+        $window.location.reload();
+    };
+
+    $scope.nextQuestion = nextQuestion;
+    $scope.quit = quit;
     });
 })();
